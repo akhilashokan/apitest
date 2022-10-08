95b69947c94c5f55c9059ef7c38a37cb740d4b9c
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-10-08 12:03:19 +0530

    +send email +csv edit

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,13 +1,13 @@
 var formidable = require('formidable');
 const { parse } = require("csv-parse");
+const nodemailer = require('nodemailer')
 const express = require("express")
 const router = express.Router()
 const sql = require('mysql')
 const fs = require("fs");
-
 const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
-
+require('dotenv').config()
 router.get("/newsletter", (req, res) => {
     res.sendFile(appDir + '/public/index.html')
 })
@@ -20,12 +20,62 @@ router.post("/newsletter", (req, res) => {
         res.end();
         fs.createReadStream(files.csv.filepath)
             .pipe(parse({ delimiter: ",", from_line: 2 }))
-            .on("data", function (row) {
-                console.log(row);
-
+            .on("data", async function (row) {
+                let email = row[0]
+                let newsletterContent = row[1]
+                let newsletterName = row[2]
+                check_user_exists(email, newsletterContent, newsletterName)
             })
 
     })
 })
 
 module.exports = router
+
+function check_user_exists(email, content, name) {
+    let con = sql.createConnection({
+        host: "localhost",
+        database: "testdb",
+        user: 'root',
+        password: ''
+    })
+    var query = 'SELECT * FROM `user` WHERE `email`="' + email + '"';
+    con.connect((err) => {
+        if (err) throw err
+        con.query(query, (err, result) => {
+            if (err) throw err;
+            console.log(result == '');
+            if (result == undefined || result == '') {
+                console.error('User not found');
+            } else {
+                let FirstName = result[0].firstName;
+                let LastName = result[0].lastName;
+                let newContent = `${FirstName} ${LastName}, ${content}`;
+                sendMail(email, newContent, name)
+                //less secure option disabled by gmail
+            }
+
+        })
+    })
+}
+
+function sendMail(email, content, name) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    })
+
+    let options = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: name,
+        text: content
+    }
+    transporter.sendMail(options, (err, result) => {
+        if (err) throw err
+        console.log("mail send", result);
+    })
+}
